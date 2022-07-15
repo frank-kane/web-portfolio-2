e9ad5755dc8bb05c8156db808fa907661314a241
Kandy-Kane <frankmyster@gmail.com>
2022-07-14 23:54:38 -0400

    resume link

--- src/modules/navbar/navbar.js
+++ src/modules/navbar/navbar.js
@@ -7,7 +7,7 @@ const NavBar = (props)=>{
 
     return(
         <div className="navbarWrapper">
-           {/* <div className="border-animation"><h3 className="border-animation__inner">About Me</h3></div>  */}
+           <a href="https://drive.google.com/file/d/1JwnFypQSaAsocRUCcdf5bGmR_TxWCjNb/view?usp=sharing" target="blank"><div className="border-animation"><h3 className="border-animation__inner">Resume</h3></div></a> 
            <div onClick={props.scrollToPortfolio}  className="border-animation"><h3 className="border-animation__inner">Portfolio</h3></div>
            <div onClick={props.scrollToEmail} className="border-animation"><h3 className="border-animation__inner">Contact Me</h3></div>
             
