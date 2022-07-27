1850247ab10362bb5773d94a7f8b7b743ab881f9
Kandy-Kane <frankmyster@gmail.com>
2022-07-27 16:15:36 -0400

    updated resume link

--- src/modules/navbar/navbar.js
+++ src/modules/navbar/navbar.js
@@ -7,7 +7,7 @@ const NavBar = (props)=>{
 
     return(
         <div className="navbarWrapper">
-           <a href="https://drive.google.com/file/d/1JwnFypQSaAsocRUCcdf5bGmR_TxWCjNb/view?usp=sharing" target="blank"><div className="border-animation"><h3 className="border-animation__inner">Resume</h3></div></a> 
+           <a href="https://drive.google.com/file/d/1wtG25Ugi4yvxK4gJU925DGhRcC1l_XdQ/view?usp=sharing" target="blank"><div className="border-animation"><h3 className="border-animation__inner">Resume</h3></div></a> 
            <div onClick={props.scrollToPortfolio}  className="border-animation"><h3 className="border-animation__inner">Portfolio</h3></div>
            <div onClick={props.scrollToEmail} className="border-animation"><h3 className="border-animation__inner">Contact Me</h3></div>
             
